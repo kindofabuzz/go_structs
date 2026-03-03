61f6c1c409210f5c82eeca827f08874b4798050a
kindofabuzz <kindofabuzz@gmail.com>
2026-03-03 02:50:53 -0600

    Refactor user creation and output methods for clarity and efficiency

--- structs.go
+++ structs.go
@@ -12,15 +12,22 @@ type user struct {
 
 func (u user) outPutDetails() {
 	fmt.Println(u.firstName, u.lastName, u.birthdate)
-	// fmt.Println(u.lastName)
-	// fmt.Println(u.birthdate)
+
 }
 
-func (u *user) clearUserName() {  // must make user a pointer to change struct
+func (u *user) clearUserName() { // must make user a pointer to change struct
 	u.firstName = "deleted"
 	u.lastName = "deleted"
 	u.birthdate = "deleted"
 
+}
+
+func newUser(firstName, lastName, birthdate string) *user {
+	return &user{
+		firstName: firstName,
+		lastName:  lastName,
+		birthdate: birthdate,
+	}
 }
 
 func main() {
@@ -28,13 +35,9 @@ func main() {
 	userLastName := getUserData("Enter your last name: ")
 	userBirthdate := getUserData("Enter your birthdate: ")
 
-	var appUser user
+	var appUser *user
 
-	appUser = user{
-		firstName: userFirstName,
-		lastName:  userLastName,
-		birthdate: userBirthdate,
-	}
+	appUser = newUser(userFirstName, userLastName, userBirthdate)
 
 	appUser.outPutDetails()
 	appUser.clearUserName()
